1128b3327d5d563dca34eb0e151421369d00c200
Mickael Lanfranchi <58193062+cecceccu@users.noreply.github.com>
2021-11-19 09:24:26 +0100

    First commit

---
+++ currentweather/currentweather/src/main/java/com/weather/lanfranchi/currentweather/CurrentweatherApplication.java
@@ -0,0 +1,13 @@
+package com.weather.lanfranchi.currentweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentweatherApplication.class, args);
+	}
+
+}
